d9d5bc002acd4c1c41c7ceb5b517272b836ce95d
hp <hp@PC>
2020-01-21 20:13:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day_2_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Tampa");
+        }
+    }
+}
